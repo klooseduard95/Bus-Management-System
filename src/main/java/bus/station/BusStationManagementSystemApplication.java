1edc5b1fa43bac8c20bc61722cc1ec0ab3317182
klooseduard95 <eduard.kloos@stud.ubbcluj.ro>
2025-10-21 16:53:05 +0300

    Initial commit

---
+++ src/main/java/bus/station/BusStationManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package bus.station;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusStationManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusStationManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bus/station/BusStationManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package bus.station;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusStationManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
