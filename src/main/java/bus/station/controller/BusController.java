39a24314a935e25eef60bb0626173fd2b7dfa67a
Jarda Mihai <mihai.jarda@stud.ubbcluj.ro>
2025-10-28 08:33:37 +0200

    Added basic Controllers

---
+++ src/main/java/bus/station/controller/BusController.java
@@ -0,0 +1,9 @@
+package bus.station.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+
+public class BusController {
+
+}
---
+++ src/main/java/bus/station/controller/PassengerController.java
@@ -0,0 +1,8 @@
+package bus.station.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+
+public class PassengerController {
+}
