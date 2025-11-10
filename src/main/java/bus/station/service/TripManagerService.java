d141ec9dd14ae6a978d91a6c32cf3a12bb865dcb
Jarda Mihai <mihai.jarda@stud.ubbcluj.ro>
2025-11-10 12:51:36 +0200

    Created Repo and Service for Driver and TripManager

---
+++ src/main/java/bus/station/service/TripManagerService.java
@@ -0,0 +1,35 @@
+package bus.station.service;
+
+import bus.station.model.TripManager;
+import bus.station.repository.TripManagerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TripManagerService {
+
+    private final TripManagerRepository tripManagerRepository;
+    @Autowired
+    public TripManagerService(TripManagerRepository tripManagerRepository) {
+        this.tripManagerRepository = tripManagerRepository;
+    }
+
+    public List<TripManager> findAll() {
+        return tripManagerRepository.findAll();
+    }
+
+    public Optional<TripManager> findManagerById(String id) {
+        return  tripManagerRepository.findById(id);
+    }
+
+    public TripManager save(TripManager tripManager) {
+        return tripManagerRepository.save(tripManager);
+    }
+
+    public void delete(String id) {
+        tripManagerRepository.deleteById(id);
+    }
+}
